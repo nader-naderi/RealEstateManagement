8f529f766a0b8e4c2e90116c0c9343eaac6449f4
Nader Naderi <2200stellaris@gmail.com>
2024-03-12 01:27:44 +0330

    Windows Client added - minor positionsing improved - persian texts added

---
+++ RSM_WindowsClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RSM_WindowsClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
